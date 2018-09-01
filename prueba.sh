03cc95a25c869e39140bf92017352e5553e621fd
Carolitta <daylic.fi@gmail.com>
2018-08-31 21:39:32 -0500

    Subiendo mis últimos cambios.

--- prueba.sh
+++ prueba.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 listado=$(lsusb | awk '{print $6 }')
 name=listado=$(lsusb | awk '{ print $7 }')
 
@@ -12,14 +13,25 @@ for dispositivos in $listado; do
 		echo "match"
 elif [ "$dispositivos" == "$cad2" ]
 	then
-		echo -e "USB ($nombre) detectada en lista negra, no se va a montar"
+		echo -e "USB ($name) detectada en lista negra, no se va a montar"
 	else
 		
 	#statements
 		echo "no match"
-		echo -e "Ha ingresado la USB $nombre, es de confianza? ingrese s en caso de afirmativo"
+		echo -e "Ha ingresado la USB $name, es de confianza? ingrese s en caso de afirmativo"
 		read usb
 
+		if [[ "$usb" == "s" ]]; then
+			echo "$dispositivos" >> /bin/listaBlanca.txt
+		else 
+			echo "$dispositivos" >> /bin/listaNegra.txt
+		fi
 
+	fi
+	#touch $listaBlanca >> $dispositivos
+done
+
+
+#volumen log volumen fisico
 
 
